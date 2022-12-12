77e01cf248f7e10c7790b32b29346bf12efbbf8f
Bruno Cabral <cabral.brc@gmail.com>
2022-12-11 21:42:30 -0800

    print tensor

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
+#include <torch/torch.h>
 
 int main()
 {
-    std::cout << "Hello" << std::endl;
+    torch::Tensor x = torch::randn({3, 3});
+    std::cout << x << std::endl;
 }
