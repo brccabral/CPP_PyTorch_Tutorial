143af74c40b91de0de2f0a32c6d6eb780cfb55ad
Bruno Cabral <cabral.brc@gmail.com>
2022-12-11 23:58:36 -0800

    run an imported model

--- main.cpp
+++ main.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
+#include <vector>
 #include <torch/script.h>
 
 int main()
 {
     torch::jit::script::Module net = torch::jit::load("../models/net.pt");
     torch::Tensor x = torch::randn({1, 100});
-    std::cout << x << std::endl;
+
+    std::vector<torch::jit::IValue> input;
+    input.push_back(x);
+
+    for (int i = 0; i < 10; i++)
+    {
+        auto out = net.forward(input);
+        std::cout << out << std::endl;
+        // std::cout << typeid(out).name() << std::endl;
+    }
 }
