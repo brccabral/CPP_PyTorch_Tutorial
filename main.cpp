51ddb97ccb16cb33947000ca29355d2964561ce3
Bruno Cabral <cabral.brc@gmail.com>
2023-01-17 12:31:30 -0800

    implemented frame_prediction

--- main.cpp
+++ main.cpp
@@ -52,6 +52,8 @@ int main(int argc, char *argv[])
             std::cerr << "\nError: Blank Frame\n";
         }
 
+        frame = frame_prediction(frame, module);
+
         cv::imshow("video", frame);
         if (cv::waitKey(1) >= 27)
         {
@@ -64,8 +66,50 @@ torch::jit::Module load_model(std::string model_name)
 {
     std::string directory = "../models/" + model_name;
     torch::jit::Module module = torch::jit::load(directory);
-    // module.to(torch::CUDA);
+    //// module.to(torch::CUDA);
     module.eval();
     std::cout << "MODEL LOADED" << std::endl;
     return module;
+}
+
+cv::Mat frame_prediction(cv::Mat frame, torch::jit::Module model)
+{
+    // Needed for Overlay, layer on top of video frames
+    // highlights road lanes with a blue shade
+    double alpha = 0.4;
+    double beta = (1 - alpha);
+    cv::Mat frame_copy, dst;
+
+    // Torch model input
+    std::vector<torch::jit::IValue> input;
+
+    // Mean and Stdev
+    std::vector<double> mean = {0.406, 0.456, 0.485};  // in Python these values are reverse order
+    std::vector<double> stdev = {0.225, 0.224, 0.229}; // in Python these values are reverse order
+
+    cv::resize(frame, frame, cv::Size(IMG_SIZE, IMG_SIZE));
+    frame_copy = frame;
+    frame.convertTo(frame, CV_32FC3, 1.0f / 255.0f);
+
+    // CV2 to Tensor
+    torch::Tensor frame_tensor = torch::from_blob(frame.data, {1, IMG_SIZE, IMG_SIZE, 3});
+    frame_tensor = frame_tensor.permute({0, 3, 1, 2}); // reshape tensor
+    frame_tensor = torch::data::transforms::Normalize<>(mean, stdev)(frame_tensor);
+    //// frame_tensor = frame_tensor.to(torch::CUDA);
+    input.push_back(frame_tensor);
+
+    // Forward pass
+    //// auto pred = model.forward(input).toTensor().detach().to(torch::CUDA);
+    auto pred = model.forward(input).toTensor().detach();
+    pred = pred.mul(100).clamp(0.255).to(torch::kU8);
+
+    // Tensor to CV2
+    cv::Mat output_mat(cv::Size(IMG_SIZE, IMG_SIZE), CV_8UC1, pred.data_ptr());
+    cv::cvtColor(output_mat, output_mat, cv::COLOR_GRAY2RGB);
+    cv::applyColorMap(output_mat, output_mat, cv::COLORMAP_TWILIGHT_SHIFTED);
+
+    // Overlay original frame and prediction
+    cv::addWeighted(frame_copy, alpha, output_mat, beta, 0.0, dst);
+    cv::resize(dst, dst, cv::Size(DEAFULT_WIDTH, DEAFULT_HEIGHT));
+    return dst;
 }
